880300aa52b6a8db4e960db000914ab581c1c2e2
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2024-04-05 23:59:19 +0530

    read variable in go

--- golang_helloworld/main.go
+++ golang_helloworld/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
-	"golang/myutil"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+	// myutil.PrintMessage("Learn go lang by example!")
 
-	myutil.PrintMessage("Learn go lang by example!")
+	// var name string = "Paresh"
+	// var age = 24
+	// fmt.Println("Hello, ", name)
+	// fmt.Println("Age: ", age)
+
+	// var money int = 2400
+	// fmt.Println("Money: ", money)
+
+	// var dimension float64 = 24.5
+	// fmt.Println("Dimension: ", dimension)
+
+	// var isTrue bool = true
+	// fmt.Println("Is True: ", isTrue)
+
+	person := "Paresh Kumar Pradhan"
+	fmt.Println("Hello, ", person)
 }
